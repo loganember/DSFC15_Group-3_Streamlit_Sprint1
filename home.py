9d24f80feaead92c336156b61b8099b15be4d4de
loganember <loganember@gmail.com>
2025-09-05 23:26:12 +0000

    Updated pages

--- home.py
+++ home.py
@@ -2,3 +2,5 @@ import streamlit as st
 # Home
 st.title("From Clusters to Clarity: Predicting Churn with K-Means")
 st.subheader("Uncovering hidden patterns in churn behavior")
+st.divider(color="gray")
+st.header("Presented by Group 3: Data Miming", divider="gray")
