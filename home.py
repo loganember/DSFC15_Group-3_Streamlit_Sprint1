287ee94e9eb75a1b29f2dfb80f6b523cc39c0f62
loganember <loganember@gmail.com>
2025-09-06 00:45:46 +0000

    Updated pages

--- home.py
+++ home.py
@@ -30,6 +30,6 @@ with col2:
       st.markdown("##### However, growth is only half the story. In this competitive market, **even small amounts of churn can quietly cut into profits.**")
 
 st.divider()
-with st.container(vertical_alignment="center", width="stretch"):
+with st.container(horizontal_alignment="center", width="stretch"):
   st.markdown('''
       # :blue-background[The Big Question]''')
