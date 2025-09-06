c8a1a6fe3491edc02c2dea200502fd1dffd73d6d
loganember <loganember@gmail.com>
2025-09-06 00:29:56 +0000

    Updated pages

--- home.py
+++ home.py
@@ -20,11 +20,11 @@ with col1:
     st.image(churn)
 
 with col2:
-    st.empty()
-    st.markdown("*As of 2025, leading banks in the Philippines reported strong **double-digit growth in credit card receivables**, driven by rising consumer spending.")
-    left, right = st.columns(2)
-    left.button("+48% growth in 2024*", width="stretch")
-    right.button("+18.2% growth in H1 2025**", width="stretch")
-    st.markdown("However, growth is only half the story. In this competitive market, **even small amounts of churn can quietly cut into profits.**")
+    with st.container(vertical_alignment="center"):
+      st.markdown("*As of 2025, leading banks in the Philippines reported strong **double-digit growth in credit card receivables**, driven by rising consumer spending.")
+      left, right = st.columns(2)
+      left.button("+48% growth in 2024*", width="stretch")
+      right.button("+18.2% growth in H1 2025**", width="stretch")
+      st.markdown("However, growth is only half the story. In this competitive market, **even small amounts of churn can quietly cut into profits.**")
 
 st.divider()
