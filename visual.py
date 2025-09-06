ea5d4a2340696296339b3cd098a5178148bfe0b6
loganember <loganember@gmail.com>
2025-09-06 03:58:41 +0000

    Updated pages

--- visual.py
+++ visual.py
@@ -42,8 +42,101 @@ with st.expander("See analysis"):
         * **Top-right (positive avg, positive cum)**  Accounts steadily expanding into more categories.
         * **Top-left (negative avg, positive cum)**   Historically expanded, but recently slowing down.
         * **Bottom-right (positive avg, negative cum)** Historically contracted, but recently recovering.
-        * :red[**Bottom-left (negative avg, negative cum)** Long-term and recent contraction 
-          * Flagged Accounts]
+        * **Bottom-left (negative avg, negative cum)** Long-term and recent contraction 
+          * Flagged Accounts
+        
         **Regression line** → Shows the average relationship: if slope is positive, accounts with negative averages tend also to have negative cumulative growth.
 
   ''')
+
+st.subheader("4. Visualizing streaks of months with decline in category breadth ")
+g4 = os.path.join(plot_dir, "4.png")
+st.image(g4)
+
+st.subheader("5. Historical Benchmarking: Flagged Accounts show consistent 3-Month and 6-Month Accelerated Decline in Spending Volume")
+g5 = os.path.join(plot_dir, "5.png")
+st.image(g5)
+with st.expander("See analysis"):
+  st.write('''
+        **Blue Line – Monthly Spend**: Customer’s actual spending each month.
+        **Orange Line – 3-Month Avg**: Smooths short-term changes to show recent spending trends.
+        **Red Line – 6-Month Avg**: Longer-term baseline to separate temporary dips from true declines.
+        **Red Dots – Churn Risk Flags**:
+            Triggered when:
+              1. 3-Month Avg is falling
+              2. 6-Month Avg is falling
+              3. Monthly spend keeps dropping (negative velocity)
+
+        → **Indicates churn risk, noting consistent, accelerating decline in spending**
+
+  ''')
+
+st.subheader("6. Peer benchmarking: Creating a category churn threshold by geographic cohort")
+
+col6, col7 = st.columns(2)
+
+with col6:
+    g6 = os.path.join(plot_dir, "6.png")
+    st.image(g6)
+
+with col7:
+    g7 = os.path.join(plot_dir, "7.png")
+    st.image(g7)
+
+with st.container(height="content", vertical_alignment="center"):
+  with st.expander("See analysis"):
+    st.write('''
+          **Urban Customers are Big-Basket Shoppers.** Their average transaction size is high, but their overall monthly total is lower.
+          **Rural Customers are Small-Basket, Frequent Shoppers.** Their average transaction size is very low, but because they make so many purchases, their overall monthly total is much, much higher.
+
+    ''')
+
+st.subheader("7. Peer benchmarking: Creating a category churn threshold by geographic cohort ")
+
+col8, col9 = st.columns(2)
+
+with col8:
+    g8 = os.path.join(plot_dir, "8.png")
+    st.image(g8)
+
+with col9:
+    g9 = os.path.join(plot_dir, "9.png")
+    st.image(g9)
+
+
+st.subheader("8. Flagged Accounts show under transaction vs. segment-specific category norms, adjusted for region")
+g10 = os.path.join(plot_dir, "10.png")
+st.image(g10)
+with st.expander("See analysis"):
+  st.write('''
+        * **Flagged accounts below average median of bottom 25% for risk of churn**
+        * **Risk**: We've identified Rural accounts at high risk of churn  
+        * **Opportunity**: Conversely, our Urban customer base shows strong resilience and consistent engagement in key lifestyle categories like fitness, travel, and shopping. 
+
+        
+        Recommendation: Two-pronged strategy: targeted re-engagement campaigns for the at-risk Rural segment and up-sell/loyalty initiatives for the stable Urban core.
+  ''')
+
+st.subheader("9. Silent Generation is infrequent but high-spend, Baby Boomers are frequent by low-spend and Gen X is balanced ")
+
+col11, col12, col13 = st.columns(3)
+
+with col11:
+    g11 = os.path.join(plot_dir, "11.png")
+    st.image(g11)
+
+with col12:
+    g12 = os.path.join(plot_dir, "12.png")
+    st.image(g12)
+
+with col13:
+    g13 = os.path.join(plot_dir, "13.png")
+    st.image(g13)
+
+with st.container(height="content", vertical_alignment="center"):
+  with st.expander("See analysis"):
+    st.write('''
+          * **Baby Boomers** transact the most often (1,073) but in small amounts ($162).
+          * **Generation X** hasn’t transacted recently (102 days) but exhibit balanced spending behavior, i.e. they transact fairly frequently (924) and in fair amounts ($219).
+          * **Silent Generation** engage the most recently (58 days), don’t transact often (766) but when they do, they spend big ($263).
+    ''')
