d8c54643904f3b2e47ff597047ab0e7c7ad46e55
loganember <loganember@gmail.com>
2025-09-06 00:59:54 +0000

    Updated pages

--- game_plan.py
+++ game_plan.py
@@ -1,4 +1,30 @@
 import streamlit as st
+import os
+
+data_dir = "data"
+image_dir = "images"
+plot_dir = "plots"
+
 # Game Plan
 st.title("The Game Plan")
+with col1:
+    st.markdown('''
+      The analysis focused on the following key factors:
+        * How often Adobo Bank use their credit card/s
+        * How recently they used it
+        * How much they spend on it
+      The following methods (on the right) were used to determine which customers are at a higher risk of churn.
+
+      ''')
+
+with col2:
+    with st.container(height=400, vertical_alignment="center"):
+      st.markdown('''
+      #### Customer Churn Prediction
+      ###### This method is best for binary classification, i.e. churn or not churn. It is also able to output probabilities, for eg. Customer A has a 85% chance of churn.
+
+      #### K-Means Clustering (Machine Learning)
+      ###### After we have determined the similar usage pattern of each customer in terms of recency and frequency, they can be clustered into whether or not they will stop using their credit card. This solution can also help distinguish the temporal and behavioral patterns for the customers.
+      ''')
+
 # put divider
