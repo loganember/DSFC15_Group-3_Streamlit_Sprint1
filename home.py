e0bf9908d4a562a1ed089beeca664f08369c0088
loganember <loganember@gmail.com>
2025-09-06 00:13:18 +0000

    Updated pages

--- home.py
+++ home.py
@@ -5,3 +5,16 @@ st.subheader("Uncovering hidden patterns in churn behavior")
 
 st.write("Presented by Group 3: Data Miming")
 st.divider()
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("Why Does Churn Matter?")
+
+with col2:
+    st.markdown("*As of 2025, leading banks in the Philippines reported strong **double-digit growth in credit card receivables**, driven by rising consumer spending.")
+    left, right = st.columns(2)
+    if left.button("+48% growth in 2024*", width="stretch"):
+      left.markdown("RCBC.")
+    if right.button("+18.2% growth in H1 2025**", width="stretch"):
+      right.markdown("Metrobank")
