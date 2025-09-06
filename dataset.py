009b66a66e724a337d76059574186aa93fbb9b6f
loganember <loganember@gmail.com>
2025-09-06 03:04:18 +0000

    Updated pages

--- dataset.py
+++ dataset.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import pandas as pd
+import os
+
+data_dir = "data"
+image_dir = "images"
+plot_dir = "plots"
+
+
 # Dataset
 st.title("Adobo Bank Dataset")
+
+df = pd.read_csv('data/cc_clean.csv')
+st.dataframe(df)
